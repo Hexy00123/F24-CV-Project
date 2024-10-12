425172e850b3d5071e452356accf283590b0ceaa
Ruslan Izmailov <r.izmajlov2018@yandex.ru>
2024-10-12 22:58:38 +0300

    todo: tasks for future work documented

---
+++ vision_transformer/ImageEmbeddings.py
@@ -0,0 +1,8 @@
+# TODO: Implemet image patcher class that takes 
+# batch of images and cut it into sequence of patches 
+# (Batch, channels, height, width) -> (Batch, sequence_length, channels, height, width)
+
+
+# TODO: implement a class that would take a sequence of
+# image patches (Batch, sequence_length, channels, height, width) and return a sequence of
+# image embeddings (Batch, sequence_length, embedding_size)
---
+++ vision_transformer/PositionalEncodings.py
@@ -0,0 +1,3 @@
+# TODO: implement a class that would take a sequence of
+# image embeddings (Batch, sequence_length, embedding_size)
+# and would add a position encoding to each sequence in a batch
