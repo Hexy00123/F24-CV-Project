fc4ca676d4decdbbeb80e28399d92039eec7b54e
Israel Adewuyi <isistickz@gmail.com>
2024-10-24 18:54:18 +0300

    fix: fixed logging dir

--- src/logs.py
+++ src/logs.py
@@ -1,8 +1,23 @@
 import logging
 import os
 from datetime import datetime
+from dotenv import load_dotenv
 
-def setup_logging(log_dir='Logs'):
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the project root directory from the environment variable
+project_root = os.getenv('PROJECT_ROOT')
+if not project_root:
+    raise ValueError("PROJECT_ROOT environment variable is not set.")
+
+# Define the runs directory
+log_dir = os.path.join(project_root, 'training_logs/Logs')
+
+# Ensure the runs directory exists
+os.makedirs(log_dir, exist_ok=True)    
+
+def setup_logging(log_dir=log_dir):
     """
     Sets up logging for the project.
     """
